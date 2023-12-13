30b9516d7130c0426143071b27a84b65e02a5091
jericodings <jericosalenga@gmail.com>
2023-12-13 12:34:26 +0800

    new file:   .gitignore new file:   Cargo.lock new file:   Cargo.toml new file:   src/main.rs

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+
+const OUR_COURSE: &str = "Rust with AutoGPT";
+
+fn main() {
+    
+    println!("Welcome to this course on {}!", OUR_COURSE);
+    
+    // Stack
+    let x: i32;
+    x = 2;
+    println!("x is {}", x);
+
+    let y: i32 = 4;
+    println!("y is {}", y);
+
+    // For loop
+    for i in 0..=y {
+        if i != 4 {
+            print!("{}, ", i);
+        } else {
+            println!("{}", i);
+        }
+    }
+
+    // Mutation
+    let mut z: i32 = 5;
+    print!("z was {} ", z);
+    z = 10;
+    println!("but is now {}", z);
+
+    let freezing_temp: f64 = -2.4;
+    println!("freezing_temp is {}", freezing_temp);
+
+    let is_zero_remainder: bool = 10 % 4 != 0;
+    println!("is_zero_remainder is {}", is_zero_remainder);
+
+}

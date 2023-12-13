03e7206dc1bc5e498377dd11c2f80261a85911ad
jericodings <jericosalenga@gmail.com>
2023-12-13 14:53:24 +0800

    Rust from vector to string

--- src/main.rs
+++ src/main.rs
@@ -3,48 +3,48 @@ const OUR_COURSE: &str = "Rust with AutoGPT";
 
 fn main() {
     
-    println!("Welcome to this course on {}!", OUR_COURSE);
+    // println!("Welcome to this course on {}!", OUR_COURSE);
     
-    // Stack
-    let x: i32;
-    x = 2;
-    println!("x is {}", x);
+    // // Stack
+    // let x: i32;
+    // x = 2;
+    // println!("x is {}", x);
 
-    let y: i32 = 4;
-    println!("y is {}", y);
+    // let y: i32 = 4;
+    // println!("y is {}", y);
 
-    // For loop
-    for i in 0..=y {
-        if i != 4 {
-            print!("{}, ", i);
-        } else {
-            println!("{}", i);
-        }
-    }
+    // // For loop
+    // for i in 0..=y {
+    //     if i != 4 {
+    //         print!("{}, ", i);
+    //     } else {
+    //         println!("{}", i);
+    //     }
+    // }
 
-    // Mutation
-    let mut z: i32 = 5;
-    print!("z was {} ", z);
-    z = 10;
-    println!("but is now {}", z);
+    // // Mutation
+    // let mut z: i32 = 5;
+    // print!("z was {} ", z);
+    // z = 10;
+    // println!("but is now {}", z);
 
-    let freezing_temp: f64 = -2.4;
-    println!("freezing_temp is {}", freezing_temp);
+    // let freezing_temp: f64 = -2.4;
+    // println!("freezing_temp is {}", freezing_temp);
 
-    let is_zero_remainder: bool = 10 % 4 != 0;
-    println!("is_zero_remainder is {}", is_zero_remainder);
+    // let is_zero_remainder: bool = 10 % 4 != 0;
+    // println!("is_zero_remainder is {}", is_zero_remainder);
 
-    let my_char: char = 'z';
-    println!("my_char is {}", my_char);
+    // let my_char: char = 'z';
+    // println!("my_char is {}", my_char);
 
-    let emoji_char: char = '😎';
-    println!("emoji_char is {}", emoji_char);
+    // let emoji_char: char = '😎';
+    // println!("emoji_char is {}", emoji_char);
 
-    let my_floats: [f32; 10] = [0.0; 10];
-    println!("my_floats is {:?}", my_floats);
+    // let my_floats: [f32; 10] = [0.0; 10];
+    // println!("my_floats is {:?}", my_floats);
 
-    let my_floats_new: [f32; 10] = my_floats.map(|n| n + 2.0);
-    println!("my_floats_new is {:?}", my_floats_new);
+    // let my_floats_new: [f32; 10] = my_floats.map(|n| n + 2.0);
+    // println!("my_floats_new is {:?}", my_floats_new);
 
     let name: &str = "Jeric";
     println!("name is {:?}", name);
@@ -69,5 +69,21 @@ fn main() {
     let removed_char: char = chars.pop().unwrap();
     println!("removed_char is {:?}", removed_char);
     println!("chars is {:?}", chars);
+
+    // chars.iter().for_each(|c| print!("{}", c));
+
+    let chars_again: Vec<char> = vec!('h','e','l','l','o');
+    dbg!(&chars_again);
+
+    let collected: String = chars_again.iter().collect();
+    dbg!(collected);
+
+    for c in chars_again {
+        print!("{}", c);
+        if c == 'o' {
+            println!(", world!");
+        }
+    }
+
 
 }

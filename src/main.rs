20fdbfa76a039ed036036b471c96e1ccfe45d061
jericodings <jericosalenga@gmail.com>
2023-12-17 12:16:31 +0800

    Played with Binary access

--- src/main.rs
+++ src/main.rs
@@ -103,4 +103,34 @@ fn main() {
     let text: &str = r#"{\"message" : "Rust is Awesome"}"#;
     dbg!(text);
 
+    // Binary
+    let a: u8 = 0b_1010_1010;    
+    let b: u8 = 0b_0101_1010;
+    println!("a's value is {}", a);
+    println!("b's value is {}", b);
+    
+    println!("a in binary {:08b}", a);
+    println!("b in binary {:08b}", b);
+    
+    // Logic Gates
+    println!("AND {:08b}", a & b);
+    println!("OR {:08b}", a | b);
+    println!("XOR {:08b}", a ^ b);
+    println!("NOT {:08b}", !a);
+
+    // Bitwise Operations
+    println!("a << 1 {:08b}", a << 1);
+    println!("a << 1 {}", a << 1);
+    println!("a >> 1 {:08b}", a >> 1);
+    println!("a >> 1 {}", a >> 1);
+
+    // Little Endian or Big Endian
+    let n: u16 = 0x1234;
+    println!("n is: {:?}", n);
+
+    let big_endian = n.to_be_bytes();
+    let little_endian = n.to_le_bytes();
+
+    println!("n in big endian: {:02X}{:02X}", big_endian[0], big_endian[1]);
+    println!("n in little endian: {:02X}{:02X}", little_endian[0], little_endian[1]);
 }

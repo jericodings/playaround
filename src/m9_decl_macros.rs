aff31bfb9bb4fd02d801cdf122c56cf009022f84
jericodings <jericosalenga@gmail.com>
2023-12-31 17:38:48 +0800

    Made a Type Macro

--- src/m9_decl_macros.rs
+++ src/m9_decl_macros.rs
@@ -36,16 +36,22 @@ mod tests {
   
 
     macro_rules! mad_skills {
-        ($x: expr) => {
-          format!("You sent an expression: {}", $x)
-        };
-      }
+        // ($x: expr) => {
+        //   format!("You sent an expression: {}", $x)
+        // };
+        ($x: ty) => {
+            match stringify!($x) {
+                "i32" => "You sent an i32 type".to_string(),
+                _ => "You sent something else".to_string(),
+            }
+        }
+    }
     
     
   #[test]
   fn tests_declarative_macro() {
 
-    let some_var: String = mad_skills!(1 + 2);
+    let some_var: String = mad_skills!(i32);
     dbg!(some_var);
 
   }

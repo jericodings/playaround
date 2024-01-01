a0fab706633e908019a0babfc5fe1c724b8e7e5c
jericodings <jericosalenga@gmail.com>
2024-01-01 09:51:07 +0800

    Applied My Function to String Macro to a Function in the Playaround

--- src/m10_proc_macros.rs
+++ src/m10_proc_macros.rs
@@ -1,10 +1,21 @@
 #[cfg(test)]
 mod test {
+  use my_proc_macro::function_to_string;
 
+  const OUTPUT: &str = "";
+
+  #[function_to_string]
+  fn some_function_for_ai_llm(_whatever_param: &str) {
+    /// This is an awesome function
+    /// We shall give it to an AI to guess and output
+    /// In a structured manner
+    println!("{}", _OUTPUT);
+  }
 
   #[test]
   fn tests_proc_macro() {
-
-    dbg!("HELLO");
+    
+    let x: &str = some_function_for_ai_llm("some_input");
+    dbg!(x);
   }
 }
